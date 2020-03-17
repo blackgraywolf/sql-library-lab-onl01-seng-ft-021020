e64c8809c04fa15c0f5554a4641fc74339f08345
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-17 18:45:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
